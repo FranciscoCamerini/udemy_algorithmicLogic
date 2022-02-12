5149824bc5341e0dab30527473d37600126ed4b9
Francisco Camerini <83650761+FranciscoCamerini@users.noreply.github.com>
2022-02-12 20:02:34 -0300

    Add files via upload

--- ws-python/cada_linha/main.py
+++ ws-python/cada_linha/main.py
@@ -1,17 +1,25 @@
 n = int(input("Qual a ordem da matriz? "))
+print(f"")
 
-mat = [[0 for x in range(n)] for x in range(n)]
-vet = [0 for x in range(n)]
+
+mat: [int] = [[0 for x in range (n)] for x in range(n)]
+vet: [int] = [0 for x in range(n)]
+
 
 for i in range(0, n):
     for j in range(0, n):
-        mat[i][j] = float(input(f"Elemento [{i},{j}]: "))
+        mat[i][j] = int(input(f"Elemento [{i},{j}]: "))
+
 
 for i in range(0, n):
+    vet[i] = 0
     for j in range(0, n):
-        if mat[i][j] > vet[i]:
-            vet[i] = mat[i][j]
+       if mat[i][j] > vet[i]:
+           vet[i] = mat[i][j]
 
-print(f"\nMAIOR ELEMENTO DE CADA LINHA: ")
+
+print(f"")
+print(f"MAIOR ELEMENTO DE CADA LINHA: ")
 for i in range(0, n):
     print(vet[i])
+print(f"")
